230165c33e1146d62909d445a68f93fbb96db13a
mzvast <mzvast@gmail.com>
2020-10-21 00:16:10 +0800

    feat demo of webpOp

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import logo from './react.svg';
-import styled from 'styled-components';
 import {Route, Switch, Link} from 'react-router-dom';
 import './Home.css';
 import {Helmet} from 'react-helmet-async';
+import {Box, styled} from 'galaco';
+const WebpOp = ({use, src, ...props}: any) => {
+    console.log('ddt::props', props);
+    //can use webp?
+    src = src + '@f_webp,q_50';
+    return <Box {...props} src={src} use={use}></Box>;
+};
+
+const Img: any = styled(Box)`
+    width: 500px;
+`;
 const Button = styled.button`
     border-radius: 3px;
     padding: 0.25em 1em;
@@ -22,6 +32,13 @@ class Home extends React.Component<{}, {}> {
                     <meta name="description" content="Home" />
                     <meta name="keywords" content="Home" />
                 </Helmet>
+                <Img
+                    use={[WebpOp, 'img']}
+                    // as="img"
+                    src={
+                        'https://vrstudio-prod.cdn.bcebos.com/vrstudio-prod/platform/vr-index/news/头图.jpg'
+                    }
+                ></Img>
                 <div>
                     <img src={logo} className="Home-logo" alt="logo" />
                     <h2>
